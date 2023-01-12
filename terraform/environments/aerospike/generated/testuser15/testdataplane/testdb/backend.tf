a98d01bc71b8e803dca771b0c1cd88e5d70c9010
arowden1986 <arowden@aerospike.com>
2023-01-12 22:07:24 +0000

    Added terrform for customer testuser15 database testdataplane

--- terraform/environments/aerospike/generated/testuser15/testdataplane/testdb/backend.tf
+++ terraform/environments/aerospike/generated/testuser15/testdataplane/testdb/backend.tf
@@ -2,7 +2,7 @@ terraform {
 	backend "s3" {
 		bucket = "ams-13536"
 		key    = "terraform-state/git_workspace/terraform/environments/aerospike/generated/testuser15/testdataplane/testdb"
-		region = "us-east01"
+		region = "us-west-2"
 	}
 	required_providers {
 		aws = "4.36.1"
